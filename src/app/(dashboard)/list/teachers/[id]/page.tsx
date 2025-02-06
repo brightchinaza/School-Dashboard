84734238f513f6cd3f2a9e88061e1a1d1a4b5b31
brightchinaza <brightochinaza96@gmail.com>
2025-02-06 22:37:57 +0100

    School Management App Single Teacher Page Design

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,8 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
+import Performance from "@/components/Performance";
 import Image from "next/image";
+import Link from "next/link";
 
 const SingleTeacherPage = () => {
   return (
@@ -67,7 +71,7 @@ const SingleTeacherPage = () => {
           {/* SMALL CARDS */}
           <div className=" flex-1 flex gap-4 justify-between flex-wrap ">
             {/* CARD */}
-            <div className="">
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
               <Image src="/singleAttendance.png" alt="" width={24} height={24} className="w-6 h-6"/>
               <div className="">
                 <h1 className="text-xl font-semibold">90%</h1>
@@ -76,7 +80,7 @@ const SingleTeacherPage = () => {
             </div>
 
               {/* CARD */}
-              <div className="">
+              <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
               <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6"/>
               <div className="">
                 <h1 className="text-xl font-semibold">2</h1>
@@ -85,7 +89,7 @@ const SingleTeacherPage = () => {
             </div>
 
               {/* CARD */}
-              <div className="">
+              <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
               <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6"/>
               <div className="">
                 <h1 className="text-xl font-semibold">6</h1>
@@ -94,7 +98,7 @@ const SingleTeacherPage = () => {
             </div>
 
               {/* CARD */}
-              <div className="">
+              <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
               <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6"/>
               <div className="">
                 <h1 className="text-xl font-semibold">6</h1>
@@ -106,11 +110,28 @@ const SingleTeacherPage = () => {
           
         </div>
         {/* BOTTOM  */}
-        <div className="">Schedule</div>
+        <div className=" mt-4 bg-white rounded-md p-4 h-[800px] ">
+          <h1>Teacher&apos;s Schedule </h1>
+          <BigCalendar />
+        </div>
       </div>
 
       {/* RIGHT */}
-      <div className="w-full xl:w-1/3">r</div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+      <div className=" bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold ">Shortcuts</h1>
+        <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+          <Link className="p-3 rounded-md bg-lamaSkyLight"  href="/">Teacher&apos;s Classes</Link>
+          <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">Teacher&apos;s Students</Link>
+          <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">Teacher&apos;s Lessons</Link>
+          <Link className="p-3 rounded-md bg-pink-50" href="/">Teacher&apos;s Exams</Link>
+          <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Teacher&apos;s Assignments</Link>
+        </div>
+      </div>
+      <Performance />
+      <Announcements />
+      
+      </div>
     </div>
   );
 };
